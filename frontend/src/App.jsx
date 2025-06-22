fade787dddec0ccc9814bf209f593f590db708e8
Jerônimo Barbieri Junior <jeronimo.barbieri@fatec.sp.gov.br>
2025-06-22 20:18:54 -0300

    feat desenvolver faq com dados do Firestore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Faq from "./pages/Faq";
 
 function App() {
-  
   return (
     <AuthProvider>
       <AppRoutes />
