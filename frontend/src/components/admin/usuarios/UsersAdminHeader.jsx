d0251628f06c442b0ea8f60677f7f1717faf29a9
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-09-14 10:48:22 -0300

    feat: adicionando botões para retornar a página

--- frontend/src/components/admin/usuarios/UsersAdminHeader.jsx
+++ frontend/src/components/admin/usuarios/UsersAdminHeader.jsx
@@ -1,9 +1,20 @@
-import { UserCog } from "lucide-react"
+import { UserCog } from "lucide-react";
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
 const UsersAdminHeader = () => {
   return (
     <div className="bg-azul text-white py-12">
       <div className="container mx-auto px-4">
+        <div className="flex gap-2 mb-4">
+          <Link
+            to="/admin"
+            className="flex items-center text-cinza hover:text-white mb-4 w-fit"
+          >
+            <ArrowLeft className="h-4 w-4" />
+            <span>Voltar para Administração</span>
+          </Link>
+        </div>
         <div className="flex items-center gap-2">
           <UserCog className="h-6 w-6 text-verde" />
           <h1 className="text-3xl font-bold">Administração de Usuários</h1>
@@ -13,7 +24,7 @@ const UsersAdminHeader = () => {
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UsersAdminHeader
+export default UsersAdminHeader;
--- frontend/src/components/perfil/PerfilHeader.jsx
+++ frontend/src/components/perfil/PerfilHeader.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "flowbite-react";
 
 const PerfilHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-azul text-white py-12">
+      <div className="container mx-auto px-4">
+        <div
+          onClick={() => navigate(-1)}
+          className="flex items-center text-cinza hover:text-white mb-4 w-fit cursor-pointer"
+        >
+          <ArrowLeft className="h-4 w-4" />
+          <span>Voltar para página anterior</span>
+        </div>
+      </div>
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold mb-4">Meu Perfil</h1>
         <p className="text-lg text-cinza">
