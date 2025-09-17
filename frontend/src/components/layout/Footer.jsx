1864edf0dfa4cd9972de03d6864114ec9869cff4
Jerônimo Barbieri Junior <jeronimo.barbieri@fatec.sp.gov.br>
2025-09-17 17:02:42 -0300

    Footer home link

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,28 +1,65 @@
-import { Link } from 'react-router-dom';
-import logotipo from '/logotipo-sem-borda.svg';
+import { Link, useLocation } from "react-router-dom";
+import logotipo from "/logotipo-sem-borda.svg";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const location = useLocation();
 
   return (
     <footer className="bg-azul text-white pt-10 pb-6">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-
           <div>
             <h3 className="text-lg font-semibold mb-4 text-verde">Navegue</h3>
             <ul className="space-y-2">
-
-              <li><Link to="/" className="text-cinza hover:text-verde transition-colors">Início</Link></li>
-
-              <li><Link to="/denuncias" className="text-cinza hover:text-verde transition-colors">Denúncias</Link></li>
-
-              <li><Link to="/nova-denuncia" className="text-cinza hover:text-verde transition-colors">Reportar Problema</Link></li>
-
-              <li><Link to="/sobre" className="text-cinza hover:text-verde transition-colors">Sobre nós</Link></li>
+              <li>
+                {" "}
+                <Link
+                  to="/"
+                  onClick={(e) => {
+                    if (location.pathname === "/") {
+                      e.preventDefault();
+                      window.scrollTo({ top: 0, behavior: "smooth" });
+                    }
+                  }}
+                  className="text-cinza hover:text-verde transition-colors"
+                >
+                  Início
+                </Link>
+              </li>
 
               <li>
-                <Link to="/duvidas" className="text-cinza hover:text-verde transition-colors">
+                <Link
+                  to="/denuncias"
+                  className="text-cinza hover:text-verde transition-colors"
+                >
+                  Denúncias
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  to="/nova-denuncia"
+                  className="text-cinza hover:text-verde transition-colors"
+                >
+                  Reportar Problema
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  to="/sobre"
+                  className="text-cinza hover:text-verde transition-colors"
+                >
+                  Sobre nós
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  to="/duvidas"
+                  className="text-cinza hover:text-verde transition-colors"
+                >
                   <span>Dúvidas</span>
                 </Link>
               </li>
@@ -31,8 +68,11 @@ const Footer = () => {
 
           <div>
             <div className="flex items-center space-x-2 mb-4 ">
-              <Link to="/" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                <img className='h-16' src={logotipo} alt="logotipo" />
+              <Link
+                to="/"
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              >
+                <img className="h-16" src={logotipo} alt="logotipo" />
               </Link>
             </div>
             <p className="text-cinza mb-4">
@@ -52,7 +92,9 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-cinza/30 mt-8 pt-6 text-center text-cinza">
-          <p>&copy; {currentYear} Cidade Integra. Todos os direitos reservados.</p>
+          <p>
+            &copy; {currentYear} Cidade Integra. Todos os direitos reservados.
+          </p>
         </div>
       </div>
     </footer>
